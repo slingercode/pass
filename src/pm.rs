c958c4c56419837d15442471fe2399688e18caa6
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2022-08-08 22:46:22 -0500

    Create .pm home directory

--- src/pm.rs
+++ src/pm.rs
@@ -1,3 +1,15 @@
+use std::fs::create_dir;
+
 pub fn set(key: String, value: String) {
-  println!("Key: {}, Value: {}", key, value);
+  match home::home_dir() {
+    Some(path) => {
+      if let Err(e) = create_dir(path.join(".pm")) {
+        println!("Could not create .pm directory: {}", e)
+      }
+
+      println!("Path{}", path.join(".pm").display());
+      println!("Key: {}, Value: {}", key, value);
+    }
+    None => { panic!("Impossible to get your home directory"); }
+  }
 }
